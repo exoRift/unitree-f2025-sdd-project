bbfcc474b5fb3155ba18d1d59201b0e22e4a6aea
Catherine Williams <156704674+willic19@users.noreply.github.com>
2025-10-01 18:22:29 -0400

    Added alerts
    Added alerts to save and restart buttons

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -7,11 +7,19 @@ export function Toolbar (): React.ReactNode {
   return (
     <header>
       <div className='bg-[#5ac864] flex flex-row text-black font-bold items-center w-full rounded'>
-        <img src={logo} alt="logo" className="max-w-20 max-h-20 pl-5"></img>
+        <img src={logo} alt="logo" className="max-w-20 max-h-20 aspect-auto pl-5"></img>
         <div className="flex flex-row justify-between w-full pl-200 pr-20">
-          <button className="hover:bg-[#44c150] hover:cursor-pointer rounded-lg h-12 w-20">Save</button>
-          <button className="hover:bg-[#44c150] hover:cursor-pointer rounded-lg h-12 w-20">Restart</button>
-          <button className="hover:bg-[#44c150] hover:cursor-pointer rounded-lg h-12 w-20">Settings</button>
+          <button className="hover:bg-[#44c150] hover:cursor-pointer rounded-lg h-12 w-20 aspect-auto" 
+          onClick={() => alert("Progress Saved")}>
+            Save
+          </button>
+          <button className="hover:bg-[#44c150] hover:cursor-pointer rounded-lg h-12 w-20 aspect-auto"
+          onClick={() => alert("Progress Restarted")}>
+            Restart
+          </button>
+          <button className="hover:bg-[#44c150] hover:cursor-pointer rounded-lg h-12 w-20 aspect-auto">
+            Settings
+          </button>
         </div>
       </div>
     </header>
