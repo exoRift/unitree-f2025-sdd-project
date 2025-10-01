685d081bb48ee48459d78a9d0f7b9d9f0c572251
Catherine Williams <156704674+willic19@users.noreply.github.com>
2025-10-01 17:04:31 -0400

    Basic styling completed
    Created the toolbox basics (inserted the logo and the three buttons called save, restart, and settings). Gave the buttons some basic formatting along with changing the toolbox background color. Adjusted the logo colors as well. Lastly, an image folder was added with the logo placed inside it.

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,10 +1,19 @@
 /**
  * The top toolbar component. User customization, workspacing, and other settings are managed here
  */
+import logo from "../images/unitree_logo.png"
+
 export function Toolbar (): React.ReactNode {
   return (
-    <header className='bg-red-500'>
-      Toolbar goes here
+    <header>
+      <div className='bg-[#5ac864] flex flex-row text-black font-bold items-center w-full rounded'>
+        <img src={logo} alt="logo" className="max-w-20 max-h-20 pl-5"></img>
+        <div className="flex flex-row justify-between w-full pl-200 pr-20">
+          <button className="hover:bg-[#44c150] hover:cursor-pointer rounded-lg h-12 w-20">Save</button>
+          <button className="hover:bg-[#44c150] hover:cursor-pointer rounded-lg h-12 w-20">Restart</button>
+          <button className="hover:bg-[#44c150] hover:cursor-pointer rounded-lg h-12 w-20">Settings</button>
+        </div>
+      </div>
     </header>
   )
 }
