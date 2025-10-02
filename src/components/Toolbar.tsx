0517e153fc7ac03481aeda53b6e17cfeb59c33c9
Catherine Williams <156704674+willic19@users.noreply.github.com>
2025-10-02 06:54:58 -0400

    Toolbar spacing changes
    Adjusted spacing on the toolbar.

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -9,8 +9,8 @@ export function Toolbar (): React.ReactNode {
   return (
     <header>
       <div className='bg-[linear-gradient(180deg,hsla(126,34%,50%,1)_0%,hsla(126,34%,39%,1)_100%)] flex flex-row text-black font-bold items-center w-full rounded'>
-        <img src={logo} alt="logo" className="max-w-20 max-h-20 aspect-auto pl-5"></img>
-        <div className="flex flex-row justify-between w-full pl-200 pr-20">
+        <img src={logo} alt="logo" className="max-w-16 max-h-16 aspect-auto ml-8"></img>
+        <div className="flex flex-row justify-between w-full ml-200 mr-18">
           {/* Save Button (with alert functionality only)*/}
           <button className="duration-200 hover:text-white hover:cursor-pointer hover:scale-110 text-lg rounded-lg h-12 w-20 aspect-auto" 
           onClick={() => alert("Progress Saved")}>
@@ -33,7 +33,7 @@ export function Toolbar (): React.ReactNode {
       {isOpen && (
         <div className="fixed inset-26 inset-x-120 bg-[#54ab5d] text-black rounded flex flex-col items-center justify-center z-50">
           <div className="justify-items-between h-1/2 w-1/2">
-            <h1 className="pb-5 text-xl font-bold">Settings</h1>
+            <h1 className="mb-5 text-xl font-bold">Settings</h1>
           {/* Checkboxes (not functional) */}
             <div className="flex flex-col">
               <label className="hover:cursor-pointer">
