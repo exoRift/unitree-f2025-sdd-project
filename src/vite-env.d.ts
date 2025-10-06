01d8a8b93ada9b0689e9505e83ad201b1217ef45
ianLewis8 <ianlewis319@gmail.com>
2025-10-06 11:07:51 -0400

    Adding mathlive for calculator input

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+import type { MathfieldElement } from 'mathlive'
+
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements {
+      'math-field': React.DetailedHTMLProps<React.HTMLAttributes<MathfieldElement>, MathfieldElement>
+    }
+  }
+}
