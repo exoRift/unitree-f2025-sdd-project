57af688a02e0325b58846f3359fba28dcda22fd5
Catherine Williams <156704674+willic19@users.noreply.github.com>
2025-10-18 21:55:18 -0400

    made basic color and font theme classes
    created a class in the index.css file for color and font themes.

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -4,26 +4,31 @@
 import React, { useState } from 'react'
 import logo from '../images/unitree_logo_noWords.png'
 
+/**
+ * Toolbar includes the logo, save button, restart button, and settings button equipped with the
+ * settings pop-up.
+ * @returns toolbar component
+ */
 export function Toolbar (): React.ReactNode {
   const [popupOpen, setPopupIsOpen] = useState(false)
   const [darkModeOn, setDarkModeOn] = useState(false)
   const [horizontalOn, setHorizonalOn] = useState(false)
   return (
     <header>
-      <div className='bg-[#B97A57] flex flex-row text-[#7B3F16] font-bold items-center w-full rounded'>
-        <img src={logo} alt='logo' className='max-w-22 max-h-22 aspect-auto ml-8' />
+      <div className='lightBg toolbarFont flex flex-row text-white font-bold items-center w-full rounded'>
+        <img src={logo} alt='logo' className='max-w-14 max-h-14 aspect-auto ml-8' />
         <p className='font-bold text-xl'>Unitree</p>
         <div className='flex flex-row justify-between w-full ml-180 mr-18'>
           {/* Save Button (with alert functionality only) */}
-          <button className='duration-200 hover:text-white hover:cursor-pointer hover:scale-110 text-lg rounded-lg h-12 w-20 aspect-auto' onClick={() => alert('Progress Saved')}>
+          <button className='duration-200 hover:text-[#7B3F16] hover:cursor-pointer hover:scale-110 text-lg rounded-lg h-12 w-20 aspect-auto' onClick={() => alert('Progress Saved')}>
             Save
           </button>
           {/* Restart Button (with alert functionality only) */}
-          <button className='duration-200 hover:text-white hover:cursor-pointer hover:scale-110 text-lg rounded-lg h-12 w-20 aspect-auto' onClick={() => alert('Progress Restarted')}>
+          <button className='duration-200 hover:text-[#7B3F16] hover:cursor-pointer hover:scale-110 text-lg rounded-lg h-12 w-20 aspect-auto' onClick={() => alert('Progress Restarted')}>
             Restart
           </button>
           {/* Settings Button (fully functional) */}
-          <button className='duration-200 hover:text-white hover:cursor-pointer hover:scale-110 text-lg rounded-lg h-12 w-20 aspect-auto' onClick={() => setPopupIsOpen(true)}>
+          <button className='duration-200 hover:text-[#7B3F16] hover:cursor-pointer hover:scale-110 text-lg rounded-lg h-12 w-20 aspect-auto' onClick={() => setPopupIsOpen(true)}>
             Settings
           </button>
         </div>
@@ -31,7 +36,7 @@ export function Toolbar (): React.ReactNode {
 
       {/* Pop-up (not functional) */}
       {popupOpen && (
-        <div className='fixed inset-26 inset-x-120 bg-[#7B3F16] text-black rounded flex flex-col items-center justify-center z-50'>
+        <div className='fixed inset-26 inset-x-120 toolbarFont lightBg text-white rounded flex flex-col items-center justify-center z-50'>
           <div className='justify-items-between h-1/2 w-1/2'>
             <h1 className='mb-5 text-xl font-bold'>Settings</h1>
             {/* Checkboxes (not functional) */}
@@ -48,7 +53,7 @@ export function Toolbar (): React.ReactNode {
           </div>
           <div>
             {/* Close Button for Pop-up (fully functional) */}
-            <button className='font-bold duration-200 hover:text-white hover:cursor-pointer hover:scale-110 text-lg rounded-lg h-12 w-20 aspect-auto' onClick={() => setPopupIsOpen(false)}>Close</button>
+            <button className='font-bold duration-200 hover:text-[#7B3F16] hover:cursor-pointer hover:scale-110 text-lg rounded-lg h-12 w-20 aspect-auto' onClick={() => setPopupIsOpen(false)}>Close</button>
           </div>
         </div>
       )}
